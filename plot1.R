abfa937b6e13ea3d684d0c30b730af8e341d55e4
Doug <d2i2k@aol.com>
2014-06-06 20:33:42 -0700

    Revised plot1.R

--- plot1.R
+++ plot1.R
@@ -1,2 +1,4 @@
 energyData <- read.csv("./data/household_power_consumption.txt")
+png("./data/plot1.png")
 hist(energyData$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red")
+dev.off()
